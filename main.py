2d0cf760817ece9ea160fe6d21214d3de912d6b6
Shuvo Nasir <shuvonasir028@gmail.com>
2021-07-10 15:10:37 +0600

    create functionality to take user input for the quiz

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ question_bank = []
 for question in question_data:
     question_bank.append(Question(question["text"], question["answer"]))
 
-QuizBrain(question_bank)
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
