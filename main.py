e0ddb577b6478f9c6846dae107956c47ed4ab380
Shuvo Nasir <shuvonasir028@gmail.com>
2021-07-10 17:36:26 +0600

    finalize the quiz game project

--- main.py
+++ main.py
@@ -9,3 +9,6 @@ for question in question_data:
 quiz = QuizBrain(question_bank)
 while quiz.still_has_question():
     quiz.next_question()
+
+print("You've completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
