538f35744a7ef1657f7b69be6f7bc8d046ac21a3
Shuvo Nasir <shuvonasir028@gmail.com>
2021-07-10 15:09:48 +0600

    add next_question() & still_has_question() methods

--- quiz_brain.py
+++ quiz_brain.py
@@ -3,8 +3,10 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = questions_list
 
-    # def next_question(self):
-    #     for question in self.question_list:
-    #         question = self.question_list[self.question_number]
-    #         input(f"Q{self.question_number}. {question.text}")
-    #         self.question_number += 1
+    def still_has_question(self):
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
